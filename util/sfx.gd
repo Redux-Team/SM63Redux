c64f95a34d037f20e91f96c8da4886282fc38bf3
Cruglet <cruglet@gmail.com>
2025-06-27 01:05:36 -0400

    Add SFX helper function
    This allows for a bit more clarity rather than just relying on Singleton for this functionality

--- util/sfx.gd
+++ util/sfx.gd
@@ -6,3 +6,7 @@ const UI_NEXT: AudioStream = preload("uid://c457x4e2iw7b")
 const UI_BACK: AudioStream = preload("uid://dv2aeiyho5qtn")
 const UI_CONFIRM: AudioStream = preload("uid://cg8o8vgknq1li")
 const UI_START: AudioStream = preload("uid://dmop45sygbxda")
+
+
+static func play(sfx: AudioStream) -> void:
+	Singleton.play_sfx(sfx)
