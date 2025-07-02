ab575ac645487e38fa7fbf77f02ffcb94b704ff4
Cruglet <cruglet@gmail.com>
2025-07-02 19:36:23 -0400

    Add percentages to volume sliders

--- game/scenes/settings_screen/audio_handler.gd
+++ game/scenes/settings_screen/audio_handler.gd
@@ -2,8 +2,11 @@ extends Node
 
 @export var music_toggle: BooleanSettingEntry
 @export var music_slider: HSlider
+@export var music_percent_label: Label
+
 @export var sfx_toggle: BooleanSettingEntry
 @export var sfx_slider: HSlider
+@export var sfx_percent_label: Label
 
 
 func _ready() -> void:
@@ -14,6 +17,7 @@ func _ready() -> void:
 	sfx_slider.value = Config.audio.sfx_volume
 	
 	_update_sliders()
+	_update_percentages()
 
 
 func _update_sliders() -> void:
@@ -22,6 +26,11 @@ func _update_sliders() -> void:
 	
 	sfx_slider.modulate = Color.WHITE if Config.audio.sfx_on else Color.DIM_GRAY
 	sfx_slider.mouse_filter = Control.MOUSE_FILTER_PASS if Config.audio.sfx_on else Control.MOUSE_FILTER_IGNORE
+
+
+func _update_percentages() -> void:
+	music_percent_label.text = "%s%%" % int(Config.audio.music_volume)
+	sfx_percent_label.text = "%s%%" % int(Config.audio.sfx_volume)
 
 
 func _on_music_toggle_value_changed(value: bool) -> void:
@@ -33,6 +42,7 @@ func _on_music_toggle_value_changed(value: bool) -> void:
 func _on_music_slider_value_changed(value: float) -> void:
 	Config.audio.music_volume = value
 	Config.apply()
+	_update_percentages()
 
 
 func _on_sfx_toggle_value_changed(value: bool) -> void:
@@ -44,6 +54,7 @@ func _on_sfx_toggle_value_changed(value: bool) -> void:
 func _on_sfx_slider_value_changed(value: float) -> void:
 	Config.audio.sfx_volume = value
 	Config.apply()
+	_update_percentages()
 
 
 func _on_sfx_slider_drag_ended(_value_changed: bool) -> void:
