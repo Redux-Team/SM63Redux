9cfac149e0276155ba51db42ae57cff190678e2d
cruglet <cruglet@gmail.com>
2025-07-07 12:59:03 -0600

    Remove setting group

--- game/components/ui/setting_entry/setting_entry.gd
+++ game/components/ui/setting_entry/setting_entry.gd
@@ -2,13 +2,8 @@ class_name SettingEntry
 extends Node
 
 signal setting_name_changed(value: StringName)
-#signal setting_group_changed(value: StringName)
 
 @export var setting_name: StringName:
 	set(sn):
 		setting_name = sn
 		setting_name_changed.emit(sn)
-@export var setting_group: StringName:
-	set(sg):
-		setting_group = sg
-		#setting_group_changed.emit(sg)
