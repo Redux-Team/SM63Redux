c54a28a6d83cf3bcf00c498514c568f2d23fd80a
cruglet <cruglet@gmail.com>
2025-07-17 20:02:29 -0400

    Add base app icons

---
+++ game/scenes/init/init.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+func _ready() -> void:
+	Config.load()
+	Config.apply()
+	
+	if true:
+		Singleton.current_input_device = Singleton.InputType.TOUCHSCREEN
+	
+	call_deferred(&"change_to_title_screen")
+
+
+func change_to_title_screen() -> void:
+	get_tree().change_scene_to_file("uid://b0wp6l07i5ime")
