a185bdf53f23e5ce713fa8017e7ab2732a4d3ccb
cruglet <cruglet@gmail.com>
2025-07-20 22:29:20 -0400

    Add config file sanity check

--- util/config/config.gd
+++ util/config/config.gd
@@ -1,6 +1,5 @@
 class_name Config
 extends Resource
-## Global singleton-like config loader and saver.
 
 static var folder_path: String = OS.get_user_data_dir()
 static var config_file_path: String = folder_path.path_join("config.tres")
@@ -24,26 +23,105 @@ static func _static_init() -> void:
 	Config.apply()
 
 
-## Save current config resource to disk
 static func save() -> void:
 	ResourceSaver.save(_current_conf, config_file_path)
 
 
-## Load config from disk if exists, then reapply input mappings
 static func load() -> void:
 	if ResourceLoader.exists(config_file_path):
-		_current_conf = ResourceLoader.load(config_file_path)
+		var loaded_config: Config = ResourceLoader.load(config_file_path)
+		
+		if _is_config_valid(loaded_config):
+			_current_conf = loaded_config
+		else:
+			_handle_broken_config()
 	
 	get_control_scheme().assign_to_map()
 
 
-## Apply all sub-configs (display, input, audio, misc)
+static func _is_config_valid(config: Config) -> bool:
+	if not config:
+		return false
+	
+	if not config.configurations:
+		return false
+	
+	var default_config: Config = ResourceLoader.load("uid://do0n8w5nirmwk")
+	if not default_config.configurations:
+		default_config.assign_configurations()
+	
+	if config.configurations.size() != default_config.configurations.size():
+		return false
+	
+	for i: int in range(config.configurations.size()):
+		var loaded_subconfig: Subconfig = config.configurations[i]
+		var default_subconfig: Subconfig = default_config.configurations[i]
+		
+		if not _subconfig_properties_match(loaded_subconfig, default_subconfig):
+			return false
+	
+	return true
+
+
+static func _subconfig_properties_match(loaded: Subconfig, default: Subconfig) -> bool:
+	if not loaded or not default:
+		return false
+	
+	if loaded.get_class() != default.get_class():
+		return false
+	
+	var loaded_properties: Array = loaded.get_property_list()
+	var default_properties: Array = default.get_property_list()
+	
+	var loaded_export_props: Array[String] = []
+	var default_export_props: Array[String] = []
+	
+	for prop: Dictionary in loaded_properties:
+		if prop.usage & PROPERTY_USAGE_STORAGE:
+			loaded_export_props.append(prop.name)
+	
+	for prop: Dictionary in default_properties:
+		if prop.usage & PROPERTY_USAGE_STORAGE:
+			default_export_props.append(prop.name)
+	
+	if loaded_export_props.size() != default_export_props.size():
+		return false
+	
+	for prop_name: String in default_export_props:
+		if not prop_name in loaded_export_props:
+			return false
+	
+	return true
+
+
+static func _handle_broken_config() -> void:
+	print("Stored config file properties do not match expected structure, resorting to default...")
+	
+	var broken_file_path: String = config_file_path + ".broken"
+	var counter: int = 1
+	
+	while FileAccess.file_exists(broken_file_path):
+		broken_file_path = config_file_path + ".broken." + str(counter)
+		counter += 1
+	
+	var file_system: DirAccess = DirAccess.open(folder_path)
+	if file_system:
+		var error: Error = file_system.rename(config_file_path.get_file(), broken_file_path.get_file())
+		if error == OK:
+			print("Renamed broken config to: ", broken_file_path)
+		else:
+			print("Failed to rename broken config file: ", error)
+	
+	_current_conf = ResourceLoader.load("uid://do0n8w5nirmwk")
+	if not _current_conf.configurations:
+		_current_conf.assign_configurations()
+
+
 static func apply() -> void:
 	for conf: Subconfig in _current_conf.configurations:
 		conf.apply()
 
 
-## Display section: get/set display-related settings
 #region Display
 class display:
 	static var max_fps: int:
@@ -61,7 +139,6 @@ class display:
 #endregion
 
 
-## Input section: access and reset control scheme
 #region Input
 static func get_control_scheme() -> ControlScheme:
 	var _input_config: InputConfig = Config._current_conf.input_config
@@ -72,7 +149,6 @@ static func reset_control_scheme() -> void:
 	var _input_config: InputConfig = Config._current_conf.input_config
 	_input_config.control_schemes[_input_config.selected_control_scheme] = ControlScheme.copy_from(_input_config.default_control_scheme)
 	Singleton.control_scheme_changed.emit()
-
 
 
 class input:
@@ -100,7 +176,6 @@ class input:
 #endregion
 
 
-## Audio section: get/set audio settings
 #region Audio
 class audio:
 	static var output_device: String:
@@ -121,7 +196,6 @@ class audio:
 #endregion
 
 
-## Misc section: get/set miscellaneous game settings
 #region Miscellaneous
 class misc:
 	static var show_timer: bool:
