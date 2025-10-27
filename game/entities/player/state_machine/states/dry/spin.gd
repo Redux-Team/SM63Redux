c2587e0e7d6783a546dc7a3346c90792ce19ebbb
Cruglet <cruglet@gmail.com>
2025-10-27 18:41:33 -0400

    Fix gravity being reset after spin exit

--- game/entities/player/state_machine/states/dry/spin.gd
+++ game/entities/player/state_machine/states/dry/spin.gd
@@ -14,15 +14,18 @@ func _on_enter(_from: StringName) -> void:
 		else:
 			player.velocity.y -= 50
 		await get_tree().create_timer(0.1).timeout
-		player.has_gravity = true
+		if is_active():
+			player.has_gravity = true
 	
 	await get_tree().create_timer(0.5).timeout
-	player.is_spinning = false
+	if is_active():
+		player.is_spinning = false
 
 
 func _physics_process(_delta: float) -> void:
 	if player.is_on_floor():
 		player.lock_flipping = false
+	
 	player.velocity.y = min(player.velocity.y, 270)
 
 
