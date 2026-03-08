0790e8b2c4dfcd67257f2b7ca7597f5646de8a89
cruglet <cruglet@gmail.com>
2026-03-08 19:39:34 -0400

    Add prototype scene

---
+++ proto/legacy_tutorial_1/tutorial_1_1.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _on_r_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_r_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_r_4_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_title_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://game/scenes/init/init.tscn")
