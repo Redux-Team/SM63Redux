18e5a263c8d95f0bf1a829be00bef8c60978003c
cruglet <cruglet@gmail.com>
2026-03-12 11:19:10 -0400

    Create skeleton for EditorObject

--- level_designer/objects/editor_object.gd
+++ level_designer/objects/editor_object.gd
@@ -2,5 +2,25 @@
 class_name EditorObject
 extends Node2D
 
-@export var texture_atlas: Dictionary[String, Texture2D]
-@export var properties: Array
+enum PlacementRules {
+	PLACE,
+	EXPAND_CENTER_X,
+	EXPAND_EDGES_X,
+}
+
+@export var placement_rules: PlacementRules = PlacementRules.PLACE
+@export var is_preview: bool = true
+
+
+## Override to define placement behavior. Return true when placement is confirmed.
+func _placement_rules() -> bool:
+	return true
+
+
+func _place() -> void:
+	is_preview = false
+
+
+## Override to define hover behavior.
+func _hover_rules() -> void:
+	pass
