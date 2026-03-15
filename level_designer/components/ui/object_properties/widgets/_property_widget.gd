4150360bef011075dae4386081215c4b5b55d47d
cruglet <cruglet@gmail.com>
2026-03-15 16:06:07 -0400

    Add range, step, arrow_step, unbound virtual hooks to LDPropertyWidget; fix exclusive reset behaviour

--- level_designer/components/ui/object_properties/widgets/_property_widget.gd
+++ level_designer/components/ui/object_properties/widgets/_property_widget.gd
@@ -15,10 +15,36 @@ func setup(prop: LDProperty, current_value: Variant) -> void:
 	_key = prop.key
 	_default_value = prop.default_value
 	_set_label(prop.label)
+	_set_range(prop.get_range())
+	_set_unbound(prop.is_unbound())
+	_set_arrow_step(prop.get_arrow_step())
+	_set_step(prop.get_step())
 	_set_value(current_value)
-	_update_reset_button(current_value)
 	if prop.exclusive:
-		_update_reset_button(_default_value)
+		_hide_reset()
+	else:
+		_update_reset_button(current_value)
+
+
+
+
+@warning_ignore("unused_parameter")
+func _set_range(limits: Vector2) -> void:
+	pass
+
+
+@warning_ignore("unused_parameter")
+func _set_step(step: float) -> void:
+	pass
+
+
+@warning_ignore("unused_parameter")
+func _set_arrow_step(step: float) -> void:
+	pass
+
+
+func _set_unbound(unbound: bool) -> void:
+	pass
 
 
 func _on_property_applied(value: Variant) -> void:
@@ -27,6 +53,7 @@ func _on_property_applied(value: Variant) -> void:
 
 func _update_reset_button(current_value: Variant) -> void:
 	pass
+
 
 func _set_label(text: String) -> void:
 	pass
