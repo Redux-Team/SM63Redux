c62dd06be00efe9303a3842a6a1285f39de4a7c0
cruglet <cruglet@gmail.com>
2026-03-15 14:45:42 -0400

    Add get_placed_selection, get_shared_properties, get/set property value with undo history

--- level_designer/components/object_handler/object_handler.gd
+++ level_designer/components/object_handler/object_handler.gd
@@ -1,7 +1,9 @@
 class_name LDObjectHandler
 extends Node
 
+
 signal selected_object_changed(new: GameObject)
+
 
 var _selected_object: GameObject
 
@@ -13,3 +15,79 @@ func get_selected_object() -> GameObject:
 func select_object(object: GameObject) -> void:
 	_selected_object = object
 	selected_object_changed.emit(object)
+
+
+func get_placed_selection() -> Array[LDObject]:
+	return LD.get_editor_viewport().get_selected_objects()
+
+
+func get_shared_properties(objects: Array[LDObject]) -> Array[LDProperty]:
+	if objects.is_empty():
+		return []
+	
+	if objects.size() == 1:
+		return objects[0]._properties
+	
+	var result: Array[LDProperty] = []
+	for prop: LDProperty in objects[0]._properties:
+		if prop.exclusive:
+			continue
+		var shared: bool = true
+		for i: int in range(1, objects.size()):
+			var found: bool = false
+			for other_prop: LDProperty in objects[i]._properties:
+				if other_prop.key == prop.key:
+					found = true
+					break
+			if not found:
+				shared = false
+				break
+		if shared:
+			result.append(prop)
+	
+	return result
+
+
+func get_property_value(objects: Array[LDObject], key: StringName) -> Variant:
+	if objects.is_empty():
+		return null
+	
+	var first: Variant = objects[0].get_property(key)
+	for i: int in range(1, objects.size()):
+		if objects[i].get_property(key) != first:
+			return _get_property_default(objects[0], key)
+	return first
+
+
+func set_property_on_selection(key: StringName, value: Variant) -> void:
+	var objects: Array[LDObject] = get_placed_selection()
+	if objects.is_empty():
+		return
+	
+	var old_values: Array[Variant] = []
+	for obj: LDObject in objects:
+		old_values.append(obj.get_property(key))
+	
+	var history: LDHistoryHandler = LD.get_history_handler()
+	history.begin_action("Set Property: " + key)
+	history.add_do(func() -> void:
+		for obj: LDObject in objects:
+			if is_instance_valid(obj):
+				obj.set_property(key, value)
+	)
+	history.add_undo(func() -> void:
+		for i: int in objects.size():
+			if is_instance_valid(objects[i]):
+				objects[i].set_property(key, old_values[i])
+	)
+	history.commit_action()
+	
+	for obj: LDObject in objects:
+		obj.set_property(key, value)
+
+
+func _get_property_default(obj: LDObject, key: StringName) -> Variant:
+	for prop: LDProperty in obj._properties:
+		if prop.key == key:
+			return prop.default_value
+	return null
