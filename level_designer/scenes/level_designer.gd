3972cfacafcc16e24cfe7bc07a0f591754605e39
cruglet <cruglet@gmail.com>
2026-03-21 19:58:00 -0400

    Register SaveLoadHandler in level designer scene, add LD.get_save_load_handler()

--- level_designer/scenes/level_designer.gd
+++ level_designer/scenes/level_designer.gd
@@ -10,6 +10,7 @@ static var _inst: LD
 @export var _ld_tool_handler: LDToolHandler
 @export var _ld_music_handler: LDMusicHandler
 @export var _ld_history_handler: LDHistoryHandler
+@export var _ld_save_load_handler: LDSaveLoadHandler
 @export var _ld_viewport: LDViewport
 @export var _ld_ui: LDUI
 
@@ -34,16 +35,20 @@ static func get_music_handler() -> LDMusicHandler:
 	return get_instance()._ld_music_handler
 
 
+static func get_save_load_handler() -> LDSaveLoadHandler:
+	return get_instance()._ld_save_load_handler
+
+
+static func get_history_handler() -> LDHistoryHandler:
+	return get_instance()._ld_history_handler
+
+
 static func get_editor_viewport() -> LDViewport:
 	return get_instance()._ld_viewport
 
 
 static func get_ui() -> LDUI:
 	return get_instance()._ld_ui
-
-
-static func get_history_handler() -> LDHistoryHandler:
-	return get_instance()._ld_history_handler
 
 
 static func is_ready() -> bool:
