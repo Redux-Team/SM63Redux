37fd1ab55cb32087e42446d317171a7e2e0d132e
cruglet <cruglet@gmail.com>
2026-03-22 02:02:15 -0400

    New SFX system
    This was actually just ported from a local branch I had laying around

--- util/audio/sfx.gd
+++ util/audio/sfx.gd
@@ -1,13 +1,15 @@
 class_name SFX
 extends Resource
 
+
 enum TerrainType {
 	GENERIC,
 	GRASS,
 	SAND,
 	SNOW,
-	CLOUD
+	CLOUD,
 }
+
 
 const UI_NEXT: AudioStream = preload("uid://c457x4e2iw7b")
 const UI_BACK: AudioStream = preload("uid://dv2aeiyho5qtn")
@@ -15,5 +17,43 @@ const UI_CONFIRM: AudioStream = preload("uid://cg8o8vgknq1li")
 const UI_START: AudioStream = preload("uid://dmop45sygbxda")
 
 
-static func play(sfx: AudioStream) -> void:
-	Singleton._play_sfx(sfx)
+static func play(source: Variant, bus: StringName = &"SFX") -> void:
+	if source is SFXBank:
+		(source as SFXBank).play_sfx(bus)
+	elif source is AudioStream:
+		_play_stream(source, bus)
+
+
+static func play_at(source: Variant, at: Variant, bus: StringName = &"Master") -> void:
+	if source is SFXBank:
+		(source as SFXBank).play_sfx_at(at, bus)
+	elif source is AudioStream:
+		_play_stream_at(source, at, bus)
+
+
+static func stop_group(group: StringName) -> void:
+	for bank: SFXBank in SFXBank.all_banks:
+		if bank.is_in_group(group):
+			bank.stop_all()
+
+
+static func _play_stream(stream: AudioStream, bus: StringName) -> void:
+	var player: AudioStreamPlayer = AudioStreamPlayer.new()
+	player.stream = stream
+	player.bus = bus
+	player.finished.connect(player.queue_free)
+	Singleton.add_child(player)
+	player.play()
+
+
+static func _play_stream_at(stream: AudioStream, at: Variant, bus: StringName) -> void:
+	var player: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
+	player.stream = stream
+	player.bus = bus
+	player.finished.connect(player.queue_free)
+	if at is Node2D:
+		(at as Node2D).add_child(player)
+	else:
+		Singleton.add_child(player)
+		player.global_position = at as Vector2
+	player.play()
--- util/audio/sfx_bank.gd
+++ util/audio/sfx_bank.gd
@@ -27,72 +27,115 @@ enum PlayOrder {
 @export_group("Entity")
 @export var stop_on_state_exit: bool = false
 
-static var _all_banks: Array[SFXBank] = []
+static var all_banks: Array[SFXBank] = []
 
 var _current_index: int = 0
 var _shuffled_indices: Array[int] = []
 var _last_played_index: int = -1
-var _active_players: Array[AudioStreamPlayer] = []
+var _active_players: Array[Node] = []
 var _last_play_time: float = -INF
 
 
 func _init() -> void:
-	if not _all_banks.has(self):
-		_all_banks.append(self)
+	if not all_banks.has(self):
+		all_banks.append(self)
 
 
-func play_sfx() -> void:
+func play_sfx(bus: StringName = &"Master") -> void:
 	var current_time: float = Time.get_ticks_msec() / 1000.0
 	if current_time - _last_play_time < interval:
 		return
-	
 	if overwrite_group:
 		_stop_group_banks()
-	
 	var effects: Array[AudioStream] = _get_active_sound_effects()
 	if effects.is_empty():
 		return
-	
 	var index: int = _get_next_index(effects)
 	if index == -1:
 		return
-	
 	_last_played_index = index
 	_last_play_time = current_time
-	
-	var player: AudioStreamPlayer = _get_available_player()
+	var player: AudioStreamPlayer = _get_available_global_player()
 	if player == null:
 		return
-	
 	player.stream = effects[index]
+	player.bus = bus
+	player.play()
+
+
+func play_sfx_at(at: Variant, bus: StringName = &"Master") -> void:
+	var current_time: float = Time.get_ticks_msec() / 1000.0
+	if current_time - _last_play_time < interval:
+		return
+	if overwrite_group:
+		_stop_group_banks()
+	var effects: Array[AudioStream] = _get_active_sound_effects()
+	if effects.is_empty():
+		return
+	var index: int = _get_next_index(effects)
+	if index == -1:
+		return
+	_last_played_index = index
+	_last_play_time = current_time
+	var player: AudioStreamPlayer2D = _get_available_2d_player()
+	if player == null:
+		return
+	if at is Node2D:
+		var target: Node2D = at as Node2D
+		if player.get_parent() != target:
+			if player.is_inside_tree():
+				player.reparent(target)
+			else:
+				target.add_child(player)
+	else:
+		if not player.is_inside_tree():
+			Singleton.add_child(player)
+		player.global_position = at as Vector2
+	player.stream = effects[index]
+	player.bus = bus
 	player.play()
 
 
 func stop_all() -> void:
-	for player: AudioStreamPlayer in _active_players:
-		if player != null and player.playing:
-			player.stop()
+	for player: Node in _active_players:
+		if player is AudioStreamPlayer and (player as AudioStreamPlayer).playing:
+			(player as AudioStreamPlayer).stop()
+		elif player is AudioStreamPlayer2D and (player as AudioStreamPlayer2D).playing:
+			(player as AudioStreamPlayer2D).stop()
 
 
 func is_in_group(group: StringName) -> bool:
 	return bank_group == group or group == &"All" or bank_group == &"All"
 
 
+func reset() -> void:
+	_current_index = 0
+	_shuffled_indices.clear()
+	_last_played_index = -1
+	_last_play_time = -INF
+	stop_all()
+
+
+func cleanup() -> void:
+	for player: Node in _active_players:
+		if player != null:
+			player.queue_free()
+	_active_players.clear()
+	all_banks.erase(self)
+
+
 func _stop_group_banks() -> void:
-	for bank: SFXBank in _all_banks:
+	for bank: SFXBank in all_banks:
 		if bank != self and bank != null and bank.is_in_group(bank_group):
 			bank.stop_all()
 
 
 func _get_active_sound_effects() -> Array[AudioStream]:
 	var terrain_effects: Array[AudioStream] = _get_terrain_effects()
-	
 	if terrain == SFX.TerrainType.GENERIC:
 		return sound_effects
-	
 	if terrain_exclusive:
 		return terrain_effects
-	
 	var combined: Array[AudioStream] = []
 	combined.append_array(sound_effects)
 	combined.append_array(terrain_effects)
@@ -113,67 +156,70 @@ func _get_terrain_effects() -> Array[AudioStream]:
 			return []
 
 
-func _get_available_player() -> AudioStreamPlayer:
-	for player: AudioStreamPlayer in _active_players:
-		if player != null and not player.playing:
-			return player
-	
+func _get_available_global_player() -> AudioStreamPlayer:
+	for player: Node in _active_players:
+		if player is AudioStreamPlayer and not (player as AudioStreamPlayer).playing:
+			return player as AudioStreamPlayer
 	if _active_players.size() < max_stack:
 		var player: AudioStreamPlayer = AudioStreamPlayer.new()
 		player.finished.connect(_on_player_finished.bind(player))
 		Singleton.add_child(player)
 		_active_players.append(player)
 		return player
-	
 	return null
 
 
-func _on_player_finished(player: AudioStreamPlayer) -> void:
+func _get_available_2d_player() -> AudioStreamPlayer2D:
+	for player: Node in _active_players:
+		if player is AudioStreamPlayer2D and not (player as AudioStreamPlayer2D).playing:
+			return player as AudioStreamPlayer2D
+	if _active_players.size() < max_stack:
+		var player: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
+		player.finished.connect(_on_player_finished.bind(player))
+		_active_players.append(player)
+		return player
+	return null
+
+
+func _on_player_finished(player: Node) -> void:
 	if stop_on_state_exit:
 		return
-	
-	if player != null and not player.playing:
-		player.stream = null
+	if player is AudioStreamPlayer:
+		(player as AudioStreamPlayer).stream = null
+	elif player is AudioStreamPlayer2D:
+		(player as AudioStreamPlayer2D).stream = null
 
 
 func _get_next_index(effects: Array[AudioStream]) -> int:
 	match play_order:
 		PlayOrder.RANDOM:
 			return randi() % effects.size()
-		
 		PlayOrder.RANDOM_ONCE:
 			if _shuffled_indices.is_empty():
 				_shuffled_indices = _create_shuffled_indices(effects)
-			
 			if _current_index >= _shuffled_indices.size():
 				if not repeat_list:
 					return -1
 				_current_index = 0
 				_shuffled_indices = _create_shuffled_indices(effects)
-			
 			var index: int = _shuffled_indices[_current_index]
 			_current_index += 1
 			return index
-		
 		PlayOrder.RANDOM_NEW:
 			if effects.size() == 1:
 				return 0
-			
 			var index: int = randi() % effects.size()
 			while index == _last_played_index:
 				index = randi() % effects.size()
 			return index
-		
 		PlayOrder.SEQUENTIAL:
 			if _current_index >= effects.size():
 				if not repeat_list:
 					return -1
 				_current_index = 0
-			
 			var index: int = _current_index
 			_current_index += 1
 			return index
-	
 	return -1
 
 
@@ -181,22 +227,5 @@ func _create_shuffled_indices(effects: Array[AudioStream]) -> Array[int]:
 	var indices: Array[int] = []
 	for i: int in effects.size():
 		indices.append(i)
-	
 	indices.shuffle()
 	return indices
-
-
-func reset() -> void:
-	_current_index = 0
-	_shuffled_indices.clear()
-	_last_played_index = -1
-	_last_play_time = -INF
-	stop_all()
-
-
-func cleanup() -> void:
-	for player: AudioStreamPlayer in _active_players:
-		if player != null:
-			player.queue_free()
-	_active_players.clear()
-	_all_banks.erase(self)
