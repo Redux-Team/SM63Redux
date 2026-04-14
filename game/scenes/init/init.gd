70feb0740825ea72967c22b24cc810573421935d
cruglet <cruglet@gmail.com>
2026-04-14 14:42:27 -0400

    Change project startup scene to the level designer

--- game/scenes/init/init.gd
+++ game/scenes/init/init.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _ready() -> void:
-	get_tree().change_scene_to_file.call_deferred("res://game/scenes/title_screen/title_screen.tscn")
+	get_tree().change_scene_to_file.call_deferred("uid://cf4yw3eqr2qo6")
