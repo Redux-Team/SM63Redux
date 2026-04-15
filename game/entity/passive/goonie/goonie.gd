1783e61d9afd402693d1cdf06bbb51db00f1e7d8
cruglet <cruglet@gmail.com>
2026-04-14 20:10:08 -0400

    Clean up goonie riding

--- game/entity/passive/goonie/goonie.gd
+++ game/entity/passive/goonie/goonie.gd
@@ -8,9 +8,26 @@ extends Entity
 @export var glide_angle: float = 28
 @export var glide_x_boost: float = 1.3
 
-var riders: Dictionary[Variant, bool]
+var bodies: Dictionary[Entity, bool]
 
 
 func _ready() -> void:
 	velocity.x = speed * cos(deg_to_rad(flap_angle))
 	velocity.y = -speed * sin(deg_to_rad(flap_angle))
+
+
+func has_rider() -> bool:
+	for rider: Entity in bodies:
+		if rider.is_on_floor():
+			return true
+	return false
+
+
+func get_riders() -> Array[Entity]:
+	var riders: Array[Entity]
+	
+	for rider: Entity in bodies:
+		if rider.is_on_floor():
+			riders.append(rider)
+	
+	return riders
--- game/entity/passive/goonie/state_machine/processes/rider_handler.gd
+++ game/entity/passive/goonie/state_machine/processes/rider_handler.gd
@@ -2,10 +2,8 @@ extends StateProcess
 
 func _on_ride_area_body_entered(body: Node2D) -> void:
 	if body and body != owner and body is Entity:
-		owner.riders.set(body, true)
-		print(owner.riders)
+		owner.bodies.set(body, true)
 
 
 func _on_ride_area_body_exited(body: Node2D) -> void:
-	owner.riders.erase(body)
-	print(owner.riders)
+	owner.bodies.erase(body)
