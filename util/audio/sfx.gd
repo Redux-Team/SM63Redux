be110bb433a8aae4077d74a2419df3df11d83cf3
cruglet <cruglet@gmail.com>
2026-04-18 18:53:03 -0400

    Playtest world environment

--- util/audio/sfx.gd
+++ util/audio/sfx.gd
@@ -15,6 +15,7 @@ const UI_NEXT: AudioStream = preload("uid://c457x4e2iw7b")
 const UI_BACK: AudioStream = preload("uid://dv2aeiyho5qtn")
 const UI_CONFIRM: AudioStream = preload("uid://cg8o8vgknq1li")
 const UI_START: AudioStream = preload("uid://dmop45sygbxda")
+#const UI_WINDOW_OPEN: AudioStream
 
 
 static func play(source: Variant, bus: StringName = &"SFX") -> void:
