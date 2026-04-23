bc07a79ffb2f754abe2718a58efc7e1f9efb0888
cruglet <cruglet@gmail.com>
2026-04-23 17:52:12 -0400

    Add Tipping Platform (Cold)

--- game/runtime/level_layer.gd
+++ game/runtime/level_layer.gd
@@ -2,7 +2,5 @@ class_name LevelLayer
 extends Node2D
 
 
-var layer_id: String = ""
-var absolute_index: int = 0
-var relative_index: int = 0
+var layer_index: int = 0
 var decoration_layer: bool = false
