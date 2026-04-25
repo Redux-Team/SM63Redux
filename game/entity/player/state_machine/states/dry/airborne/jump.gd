8913bc1f4311870638583918d47bbaa16752b1a4
cruglet <cruglet@gmail.com>
2026-04-25 14:24:33 -0400

    Fix(?) instant double jump bug

--- game/entity/player/state_machine/states/dry/airborne/jump.gd
+++ game/entity/player/state_machine/states/dry/airborne/jump.gd
@@ -5,5 +5,10 @@ func _on_enter(_from: StringName) -> void:
 	if player.is_on_floor():
 		player.velocity.y = -player.jump_strength
 		player.jump_chain_timer = player.jump_chain_time
+		player.can_jump = false
 		await get_tree().physics_frame
 		player.current_jump += 1
+
+
+func _on_exit(_to: StringName) -> void:
+	player.can_jump = true
