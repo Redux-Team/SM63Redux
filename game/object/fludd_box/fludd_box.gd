221b382664d0abd414814a32b8815928c1f89140
cruglet <cruglet@gmail.com>
2026-05-25 01:55:22 -0400

    Fix FLUDD box opening (I think)

--- game/object/fludd_box/fludd_box.gd
+++ game/object/fludd_box/fludd_box.gd
@@ -3,21 +3,22 @@ extends LevelObject
 @export var spawn: PackedScene
 @export var sprite: SmartSprite2D
 @export var open_sfx: AudioStreamPlayer2D
-@export var player_area_check: Area2D
+
+var _opened: bool = false
 
 
 func _on_hurt_box_damaged(source_hitbox: HitBox) -> void:
+	if _opened:
+		return
+	
 	if source_hitbox.owner is Player and source_hitbox.owner.velocity.y > 0:
 		var player: Player = source_hitbox.owner
 		player.velocity.y = -200
 		open_sfx.play()
-		player_area_check.set_deferred(&"monitoring", false)
 		sprite.play(&"open")
+		_opened = true
 		
-		var object: Node2D = spawn.instantiate()
-		get_parent().add_child(object)
-		get_parent().move_child(object, get_index())
-		object.position = position
+		Singleton.spawn_sibling(self, spawn.instantiate(), ["position"])
 		
 		await sprite.animation_finished
 		
