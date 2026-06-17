4f90397e2ff3b1c2de0c2bbcc6fc02ee3c06e21e
Davin <146634489+davin-christino@users.noreply.github.com>
2026-06-17 22:21:26 +0530

    changed ground pound velocity to float from vector

--- game/entity/player/shared/states/ground_pound_start.gd
+++ game/entity/player/shared/states/ground_pound_start.gd
@@ -1,12 +1,13 @@
 @tool
 extends State
 
-@export var ground_pound_start_velocity: Vector2 = Vector2(0, -38)
+@export var ground_pound_start_velocity: float = 38
 
 func _on_enter() -> void:
-	player.velocity = ground_pound_start_velocity
+	player.velocity.y = -ground_pound_start_velocity
+	player.velocity.x = 0
 	player.current_jump = 0
 
 
 func _on_physics_tick(_delta: float) -> void:
-	player.velocity.y = ground_pound_start_velocity.y
+	player.velocity.y = -ground_pound_start_velocity
