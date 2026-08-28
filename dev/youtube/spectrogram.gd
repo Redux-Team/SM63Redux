b9fb59d3b344f777bd86a285a96e63446e17a282
Davin <146634489+davin-christino@users.noreply.github.com>
2026-08-28 13:42:13 +0530

    Use dB for bar height instead of magnitudes
    ...and cleaned up scene and script layouts

--- dev/youtube/spectrogram.gd
+++ dev/youtube/spectrogram.gd
@@ -5,29 +5,29 @@ extends Control
 var bus_id: int
 var inst: AudioEffectSpectrumAnalyzerInstance
 
-#@export_group("Attributes")
+@export_group("Attributes")
 @export var steps: int = 50
 @export var min_freq: float = 0
 @export var max_freq: float = 20000
+@export var min_db: float = -80
+@export var max_db: float = 250
 
-#@export_group("Visual")
-@export var scale_curve: Curve
-@export var height_scale: float = 20000
+@export_group("Visual")
 @export var gradient: Gradient
 @export_range(0, 1) var bar_gap_ratio: float = 0.6
 
-# Set bus layout
+# Get the audio effect instance
 func _ready() -> void:
 	#AudioServer.set_bus_layout(analysis_bus_layout)
 	bus_id = AudioServer.get_bus_index(bus)
 	inst = AudioServer.get_bus_effect_instance(bus_id, 0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Redraw every frame.
 func _process(_delta: float) -> void:
 	queue_redraw()
- 
+
+# Draw the spectrogram bars.
 func _draw() -> void:
-	
 	# Current frequency and bar position, used and modified in loop.
 	var cur_freq : float = min_freq
 	var cur_pos : float = 0
@@ -40,11 +40,23 @@ func _draw() -> void:
 	
 	# Draw each bar.
 	for i: int in steps:
+		# dB of the current frequency.
+		var bar_db : float = linear_to_db(
+				inst.get_magnitude_for_frequency_range(cur_freq, cur_freq + step_size)
+					.length()
+		)
+		#print(bar_db)
+		
+		# Convert dB into bar height:
+		var bar_height : float = remap(bar_db,
+				min_db, max_db,
+				0, size.y
+		)
+		
 		draw_rect(
 			Rect2(cur_pos, 0, 
 					bar_width * bar_gap_ratio,
-					scale_curve.sample(cur_freq) * height_scale * 
-						inst.get_magnitude_for_frequency_range(cur_freq, cur_freq + step_size).length()
+					bar_height
 			), 
 			gradient.sample(float(i) / steps)
 		)
