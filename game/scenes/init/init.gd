a4687a3c4dea0409f97484633968dd9a31f03472
cruglet <cruglet@gmail.com>
2026-09-02 02:09:20 -0400

    Remove the title screen and dev plugin

--- game/scenes/init/init.gd
+++ game/scenes/init/init.gd
@@ -1,16 +1,16 @@
 extends Control
 
-## Splash shown while the editor scene loads. It is by far the heaviest thing in a cold open, so
+## Splash shown while the level designer loads. It is by far the heaviest thing in a cold open, so
 ## it is requested on a background thread rather than paid for in a single blocking frame.
 
 
-const EDITOR_SCENE: String = "uid://cf4yw3eqr2qo6"
+const START_SCENE: String = "uid://cf4yw3eqr2qo6"
 
 
 func _ready() -> void:
-	ResourceLoader.load_threaded_request(EDITOR_SCENE)
+	ResourceLoader.load_threaded_request(START_SCENE)
 	
-	while ResourceLoader.load_threaded_get_status(EDITOR_SCENE) == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+	while ResourceLoader.load_threaded_get_status(START_SCENE) == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
 		await get_tree().process_frame
 	
-	get_tree().change_scene_to_file(EDITOR_SCENE)
+	get_tree().change_scene_to_file(START_SCENE)
--- game/scenes/title_screen/components/menu_screen.gd
+++
@@ -1,2 +0,0 @@
-class_name TitleMenuScreen
-extends Control
